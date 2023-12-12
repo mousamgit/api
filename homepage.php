e932438f02894dbf161846619ce2d5d7e794fdc5
sams-group-websites <rudy@samsgroup.com.au>
2023-12-12 16:57:39 +1100

    login-homepage

--- homepage.php
+++ homepage.php
@@ -6,6 +6,28 @@ if (!isset($_SESSION["username"])) {
     header("Location: login.php");
     exit();
 }
+else{
+    $username = $_SESSION["username"];
+}
+
+function getAttribute($sku, $attribute) {
+    require('connect.php');
+    // Construct the SQL query
+    $escapedAttribute = mysqli_real_escape_string($con, $attribute);
+    $sql = "SELECT `$escapedAttribute` FROM users WHERE username = '$username' LIMIT 1";
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        // return $row[$attribute];
+        return $row[$escapedAttribute];
+    } else {
+        // Handle query error (you might want to log or display an error message)
+        return 'not found';
+    }
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +37,7 @@ if (!isset($_SESSION["username"])) {
     <title>Homepage</title>
 </head>
 <body>
-    <h2>Welcome, <?php echo $_SESSION["type"]; ?>!</h2>
+    <h2>Welcome, <?php echo $_SESSION["username"]; ?>!</h2>
     <p>This is your homepage content.</p>
     <a href="logout.php">Logout</a>
 </body>
