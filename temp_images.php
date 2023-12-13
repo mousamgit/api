0e95c7c45d9f876bbc5da8b2e4b4519e9c68b45a
samsgroup-dan <rudy@samsgroup.com.au>
2023-12-13 12:06:45 +1100

    [dt] uploading images and check temp folder

---
+++ temp_images.php
@@ -0,0 +1,123 @@
+<html>
+<head>
+  <title>Images Uploaded</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+  <style>
+    body { font-family: 'Open Sans', sans-serif; }
+    .main-box { width:1200px; margin:0 auto; padding:50px; border:1px solid #000; background-color:#F9F6F0; text-align:center; }
+    table {border: 1px solid #000; width:100%;}
+    img { width: 100%; height:100%; }
+    th, td { width: 12.5%; height: 12.5%; border: 1px solid #000; padding:10px;}
+  </style>
+  <script>
+    $(function(){
+        $('.checkAll').click(function(){
+            $(this).closest('tr').find('input[type="checkbox"]').each(function(){
+            $(this).prop("checked", true);
+        });
+    });
+  </script>
+</head>
+<body>
+
+<div class="main-box">
+<form action="approve_temp_images.php" method="post" name="approve_images" enctype="multipart/form-data">
+
+    <table>
+        <tr>
+            <th>SKU</th>
+            <th>Image 1</th>
+            <th>Image 2</th>
+            <th>Image 3</th>
+            <th>Image 4</th>
+            <th>Image 5</th>
+            <th>Image 6</th>
+            <th>Approve All</th>
+        </tr>
+            <?php
+                $dir = "temp-images/";
+                $files = scandir($dir);
+                unset($files[0]);
+                unset($files[1]);
+
+                //var_dump(array_values($files));
+
+                //get SKUs into SKU array
+                foreach ($files as $key)
+                {
+                    if(!$itemArray)
+                    {
+                        if( strpos($key,"_") > 0 ) 
+                        {
+                            $sku = strstr($key,'_',true); // find _ and remove everything after
+                            $number = substr(strstr($key,'_'),1,1);
+                        }
+                        else
+                        {
+                            $sku = strstr($key,'.',true); // find . and remove everything after
+                            $number = 1; 
+                        }
+                        $itemArray[$sku][$number] = $key;
+                    }
+                    else
+                    {
+                        if( strpos($key,"_") > 0 ) 
+                        {
+                            $sku = strstr($key,'_',true); // find _ and remove everything after
+                            $number = substr(strstr($key,'_'),1,1);
+                        }
+                        else
+                        {
+                            $sku = strstr($key,'.',true); // find . and remove everything after
+                            $number = 1; 
+                        }
+                        $itemArray[$sku][$number] = $key;
+                    }
+                    
+                }
+
+                $keys = array_keys($itemArray);
+
+                for ($i=0; $i < count($itemArray); $i++)
+                {
+                    echo "<tr>";
+                    echo "<td>".$keys[$i]."</td>";
+
+                    for ($a=1; $a<=6; $a++)//add "No Image" text to keys without images
+                    {
+                        if (empty($itemArray[$keys[$i]][$a]))
+                        {
+                            $itemArray[$keys[$i]][$a] = "No Image";
+                        }
+                    }
+                    
+                    ksort($itemArray[$keys[$i]]); // sort all keys numerically
+
+                    foreach($itemArray[$keys[$i]] as $key => $value )
+                    {
+                        if ($value != "No Image")
+                        {
+                            echo "<td>
+                                <img src='https://pim.samsgroup.info/temp-images/".$value."'>
+                                <input type='checkbox' value='".$keys[$i].":".$value."' name='check' class='".$keys[$i]."' />
+                                </td>";
+                        }
+                        else
+                        {
+                            echo "<td></td>";
+                        }
+                    }
+                    echo "<td><button type='button' name='checkAll' class='checkAll' sku='".$keys[$i]."'> Select All </button></td>";
+                    echo "</tr>";
+                }
+                
+            ?>
+    </table>
+    <br>
+    <button type="submit" id="submit" name="submit" class="btn btn-primary button-loading" data-loading-text="Loading...">Submit Approved Images</button>
+    </form>
+</div>
+</body>
+</html>
