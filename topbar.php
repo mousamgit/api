699db0b8aecd46dde097d307babd218e16cfe1db
sams-group-websites <rudy@samsgroup.com.au>
2023-12-15 15:56:34 +1100

    add topbar

---
+++ topbar.php
@@ -0,0 +1,10 @@
+<header>
+        <div class="welcome_info">Welcome, <?php echo $_SESSION["username"]; ?>!</div>
+        <div class="header_nav">
+            <a href="logout.php" >Logout</a>
+            <a href="import.php" >Import Products</a>
+            <a href="update.php" >Update Products</a>
+            <a href="profile.php" >User Profile</a>
+            <a href="homepage.php" >Home page</a>
+        </div>
+    </header>
