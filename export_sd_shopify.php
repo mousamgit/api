09584741a26075f710bf71f81eeca1db7f8adc13
S0oLee <soo@samsgroup.com.au>
2024-02-29 16:07:52 +1100

    exportsd

--- export_sd_shopify.php
+++ export_sd_shopify.php
@@ -1,15 +1,29 @@
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+  <style>
+    body { font-family: 'Open Sans', sans-serif; }
+    table { border: 2px solid #000; }
+    th { font-size: 14px; font-weight: 700; border: 1px solid #000; padding:20px 40px; }
+    td { font-size: 12px; font-weight: 400; border: 1px solid #000; padding: 20px; }
+  </style>
+
+</head>
+<body>
+<div style="margin: 0 auto; width:600px; padding:20px; background-color:#F9F6F0; text-align:center;">
+
 <?php
   $startScriptTime=microtime(TRUE);
   include_once ('connect.php');
   include_once ('mkdir.php');
 
-  $query = 'SELECT * FROM pim WHERE (image1<> "" AND retail_aud > 0 AND description<> "" AND sync_shopify=1 AND brand = "Sapphire Dreams" AND collections != "SDL" AND collections != "SDM");';
+  $query = 'SELECT * FROM pim WHERE ((image1<> "" AND image1 IS NOT NULL AND retail_aud > 0 AND description<> "" AND description IS NOT NULL AND sync_shopify=1 AND brand = "Sapphire Dreams" AND collections != "SDL" AND collections != "SDM") OR (image1<> "" AND image1 IS NOT NULL AND retail_aud > 0 AND sync_shopify=1 AND type = "loose sapphires" AND collections != "SDL" AND collections != "SDM"));';
   $result = mysqli_query($con, $query) or die(mysqli_error($con));
 
   $filepath = dirname($_SERVER['DOCUMENT_ROOT']) . '/export/sd-shopify.csv';
   $fp = fopen($filepath, 'w');
 
-  $headers = array("Variant SKU","handle","Command","Body HTML","Image Command","Inventory Available:Sapphire Dreams Head Office","Tags Command","Tags","Title","Type","Variant Cost","Variant Image","Variant Price","Variant Command","Vendor","Image Src","Status","Variant Inventory Policy","Metafield:custom.metal_colour","Metafield:custom.metal_info","Metafield:custom.stone_info","Metafield:custom.stone_shape","Metafield:title_tag","Metafield:custom.certification","Metafield:custom.specifications","Metafield:custom.stone_colour","Metafield:custom.stone_specifications","Variant Inventory Tracker","Variant Fulfillment Service");
+  $headers = array("Variant SKU","Handle","Command","Body HTML","Image Command","Inventory Available:Sapphire Dreams Head Office","Tags Command","Tags","Title","Type","Variant Cost","Variant Image","Variant Price","Variant Command","Vendor","Image Src","Status","Variant Inventory Policy","Metafield:custom.metal_colour","Metafield:custom.metal_info","Metafield:custom.stone_info","Metafield:custom.stone_shape","Metafield:title_tag","Metafield:custom.certification","Metafield:custom.specifications","Metafield:custom.stone_colour","Metafield:custom.stone_specifications","Variant Inventory Tracker","Variant Fulfillment Service");
   $header_length = count($headers);
   $csv_header = '';
   for ($i = 0; $i < $header_length; $i++) { $csv_header .= '"' . $headers[$i] . '",'; }
@@ -32,9 +46,9 @@
         else { $certification = "NO";}
 
         //Descriptions, if loose sapphire generate description else import from field description
-        if (strtolower($type) == "loose sapphires")
-          if( strtolower($treatment) == "unheated") { $description = "An unheated Australian " .  ucfirst(strtlower($shape)) . " cut " . $colour . " sapphire weighing " . $carat . "ct and measures " . $measurement . "."; }
-          else {$description = "An Australian " .  ucfirst(strtlower($shape)) . " cut " . $colour . " sapphire weighing " . $carat . "ct and measures " . $measurement . ".";  }
+        if (strtolower($row[type]) == "loose sapphires")
+          if( strtolower($row[treatment]) == "unheated") { $description = "An unheated Australian " .  $row[shape] . " cut " . $row[colour] . " sapphire weighing " . $row[carat] . "ct and measures " . $row[measurement] . "."; }
+          else {$description = "An Australian " .  $row[shape] . " cut " . $row[colour] . " sapphire weighing " . $row[carat] . "ct and measures " . $row[measurement] . ".";  }
         else { $description = $row[description];}
 
         //check images
@@ -49,18 +63,22 @@
 
         //Status - draft if steve, discontinued, wholesale only
         $status = "";
-        if ( preg_match("/steve/i", strtolower($row[collections_2]))) { $status = "draft"; }
+        if ( $row[retail_exclusive] <> 1) { $status = "draft";}
+        elseif ( preg_match("/steve/i", strtolower($row[collections_2]))) { $status = "draft"; }
         elseif ( preg_match("/discontinued/i", strtolower($row[collections_2]))) { $status = "draft"; }
         elseif ( preg_match("/wholesale_only/i", strtolower($row[collections_2]))) { $status = "draft"; }
         else { $status = "active"; }
 
         //Command - delete if 0 stock, MERGE if in stock but status is draft, MERGE if everything passes
         $command = "";
-        if ($row[shopify_qty] > 0) {
-          if ($status == "active") { $command = "MERGE";  }
-          if ($status == "draft") { $command = "MERGE"; }
-          if ($row[deletion] == 1) { $command = "DELETE"; }
-        }else { $command = "DELETE";} 
+        if ( $row[retail_exclusive] <> 1) { $command = "DELETE";}
+        else {
+          if ($row[shopify_qty] > 0) {
+            if ($status == "active") { $command = "MERGE";  }
+            if ($status == "draft") { $command = "MERGE"; }
+            if ($row[deletion] == 1) { $command = "DELETE"; }
+          }else { $command = "DELETE";} 
+        }
 
 
         // Stone price vs item price
@@ -77,6 +95,8 @@
           else {$handle = $title_mod."-".$row[colour]."-".str_replace("  "," ",str_replace("&","",$row[metal_composition]))."-".$type_mod."-".$row[sku];}
         } $handle = str_replace([" ","--"],"-",strtolower($handle));
 
+
+        //Purchase cost calculation
         $purchase_cost = "";
         if( strtolower($row[type]) == "loose sapphires" ) { $purchase_cost = $row[purchase_cost_aud] * $row[carat]; $purchase_cost = round($purchase_cost,2); } else{ $purchase_cost = $row[purchase_cost_aud]; }
 
@@ -162,15 +182,15 @@
   }
 
   fclose($fp);
-  $count = mysqli_num_rows($result) -1;
+  $count = mysqli_num_rows($result);
   date_default_timezone_set('Australia/Sydney');
-  echo "<h2>SD Export Completed</h2><br>";
-  echo "Total Products Exported to CSV: ".$count."<br>";
-  echo "File URL: <a href='https://samsgroup.info/export/sd-shopify.csv'>https://samsgroup.info/export/sd-shopify.csv</a><br><br>";
+  echo "<center><h2>SD Export Completed</h2><br>";
+  echo "Total of ".$count." Products Exported<br><br>";
+  echo "<a style='font-weight:bold;' href='https://samsgroup.info/export/sd-shopify.csv'>View on Web</a><br><br>";
   echo date("Y-m-d G:i a")."<br>";
   $endScriptTime=microtime(TRUE);
   $totalScriptTime=$endScriptTime-$startScriptTime;
-  echo 'Processed in: '.number_format($totalScriptTime, 4).' seconds<br><br>';
+  echo 'Processed in: '.number_format($totalScriptTime, 4).' seconds<br><br></center>';
 
   $error = mysqli_error($con);
   if($error != "") { print($sku."Error Occurred: ".$error."<br>"); }
@@ -178,3 +198,6 @@
 
   fclose($fp);
 ?>
+</div>
+</body>
+</html>
